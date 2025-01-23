659c2259509c5b8edf3147ebd090425f1b6098a4
kartikgupta1920 <19kartikgupta@gmail.com>
2025-01-23 12:23:55 +0530

    Change in loading feature

--- src/Components/LoadingSpinner/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../LoadingSpinner/LoadingSpinner.module.css';
 
 const LoadingSpinner = () => {
-  return <div className={styles.spinner}>Loading...</div>;
+  return <div className={styles.spinner}>...</div>;
 }
 
 export default LoadingSpinner;
